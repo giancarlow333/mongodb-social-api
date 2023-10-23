c338d149edb64d266a0f6fba0990ef2ee848269a
Giancarlo Whitaker <giancarlo.whitaker@gmail.com>
2023-10-22 17:23:10 -0700

    Add in User routes

---
+++ controllers/userController.js
@@ -0,0 +1,64 @@
+const { User, Thoughts } = require('../models');
+
+module.exports = {
+  // Get all users
+  async getAllUsers(req, res) {
+    try {
+      const users = await User.find();
+      res.json(users);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+  // Get a single user
+  async getSingleUser(req, res) {
+    try {
+      const user = await User.findOne({ _id: req.params.userId })
+        .select('-__v');
+
+      if (!user) {
+        return res.status(404).json({ message: 'No user with that ID' });
+      }
+
+      res.json(user);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+  // create a new user
+  async createUser(req, res) {
+    try {
+      const user = await User.create(req.body);
+      res.json(user);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+  // Update an existing user
+  async updateUser(req, res) {
+    try {
+      const user = await User.findOneAndUpdate({ _id: req.params.userId });
+
+      if (!user) {
+        return res.status(404).json({ message: 'No user with that ID' });
+      }
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+  // Delete a user and associated Thoughts
+  async deleteUser(req, res) {
+    try {
+      const user = await User.findOneAndDelete({ _id: req.params.userId });
+
+      if (!user) {
+        return res.status(404).json({ message: 'No user with that ID' });
+      }
+
+      await Thoughts.deleteMany({ _id: { $in: user.applications } });
+      res.json({ message: 'User and associated Thoughts deleted!' })
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+}
